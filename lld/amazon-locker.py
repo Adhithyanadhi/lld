ddeaa98f1c05b2f69e16916a84b7b9ac96600020
AdhithyanS <adhisxtreme@gmail.com>
2026-06-26 12:49:41 +0530

    latest changes

---
+++ lld/amazon-locker.py
@@ -0,0 +1,22 @@
+"""
+
+Amazon Locker
+-------------
+
+
+Requirements:-
+1. Delivery agents deposits the parcel in an empty box in a rack
+2. Authorized user can take the contents of the code with an access token (Pass Key)
+
+
+
+Assumption:-
+1. Delivery agents is at the user's location and has the parcel in hand
+2. no need to focus on warehouse
+
+
+Out of scope:-
+1. finding the route to the user
+
+
+"""
